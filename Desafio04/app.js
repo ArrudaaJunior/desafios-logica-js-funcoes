5f403936d0fb7e69c34e07932b041bef8e4608e0
Arruda Junior <juniorarruda15@gmail.com>
2024-05-02 23:00:29 -0300

    Desafio 04 do curso lógica programação - funções e listas

---
+++ Desafio04/app.js
@@ -0,0 +1,20 @@
+// Declarando uma lista vazia chamada listaGenerica
+let listaGenerica = [];
+
+// Declarando uma lista chamada linguagensDeProgramacao e preenchendo-a com algumas linguagens de programação populares
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+// Adicionando mais algumas linguagens à lista de linguagensDeProgramacao usando o método push()
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+// Declarando uma lista chamada listaNome e preenchendo-a com alguns nomes
+let listaNome = ['Arruda', 'Thais', 'Ana'];
+
+// Imprimindo o primeiro nome da listaNome no console
+console.log(listaNome[0]);
+
+// Imprimindo o segundo nome da listaNome no console
+console.log(listaNome[1]);
+
+// Imprimindo o último nome da listaNome no console
+console.log(listaNome[listaNome.length - 1]);
